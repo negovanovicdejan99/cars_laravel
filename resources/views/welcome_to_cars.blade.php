cdedb171c1990130fcdd8d3da92fe996fb3068a5
negovanovicdejan99 <negovanovicdejan1999@gmail.com>
2020-08-28 18:33:35 +0200

    Added new welcome page, and created about page

---
+++ resources/views/welcome_to_cars.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Cars project</title>
+    </head>
+    <body>
+        <a href = "/about">About us</a>
+        
+        <h1>Welcome to Cars Project</h1>
+    </body>
+</html>
